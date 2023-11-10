09052f997ae4c7fd7121ae332e18be239aca3490
lemmyc <mrburgundy2911@gmail.com>
2023-11-10 19:34:39 +0700

    doi method cua route auth tu post thanh get

--- assets/routes/auth.route.js
+++ assets/routes/auth.route.js
@@ -2,7 +2,7 @@ const {auth, signup, signin} = require("../controller/auth.controller");
 
 const router = require("express").Router();
 
-router.route("/").post(auth);
+router.route("/").get(auth);
 router.route("/signup").post(signup);
 router.route("/signin").post(signin);
 
